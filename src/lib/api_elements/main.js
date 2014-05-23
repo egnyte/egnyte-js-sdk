db3569850cf068ebf47c4765dbe2e0b22693b0fe
ZB <ztenerowicz@egnyte.com>
2014-05-23 10:38:43 +0200

    Limited DOM changes on selection change

---
+++ src/lib/api_elements/main.js
@@ -0,0 +1,127 @@
+var oauthRegex = /access_token=([^&]+)/;
+
+var token;
+var options;
+
+var xhr = require("xhr");
+var quota = /<h1>Developer Over Qps<\/h1>/gi;
+
+
+function authenticateInplace(callback) {
+    if (!token) {
+        var access = oauthRegex.exec(window.location.hash);
+
+        if (access) {
+            if (access.length > 1) {
+
+                token = access[1];
+                callback();
+
+            } else {
+                //what now?
+            }
+        } else {
+            window.location.href = options.egnyteDomainURL + "/puboauth/token?client_id=" + options.key + "&mobile=" + ~~(options.mobile) + "&redirect_uri=" + window.location.href;
+        }
+    } else {
+        callback();
+    }
+}
+
+//TODO: implement popup flow
+function authenticateWindow(callback, pingbackURL) {
+    if (!token) {
+        var dialog = window.open(options.egnyteDomainURL + "/puboauth/token?client_id=" + options.key + "&mobile=" + ~~(options.mobile) + "&redirect_uri=" + pingbackURL);
+
+        //listen for a postmessage from window that gives you a token 
+    } else {
+        callback();
+    }
+
+}
+
+function authorizeXHR(xhr) {
+    //assuming token_type was bearer, no use for XHR otherwise, right?
+    xhr.setRequestHeader("Authorization", "Bearer " + token);
+}
+
+function getHeaders() {
+    return {
+        "Authorization": "Bearer " + token
+    };
+}
+
+function getEndpoint() {
+    return options.egnyteDomainURL + "/pubapi/v1";
+}
+
+function isAuthenticated() {
+    return !!token;
+}
+
+function getToken() {
+    return token;
+}
+
+function setToken(externalToken) {
+    token = externalToken;
+}
+
+
+function dropToken(externalToken) {
+    token = null;
+}
+
+function params(obj) {
+    var str = [];
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    }
+    return str.join("&");
+}
+
+function sendRequest(opts, callback) {
+    if (isAuthenticated()) {
+        if (opts.params) {
+            opts.url += "?" + params(opts.params);
+        }
+        opts.headers = opts.headers || {};
+        opts.headers["Authorization"] = "Bearer " + getToken();
+        return xhr(opts, function (error, response, body) {
+            try {
+                //this shouldn't be required, but server sometimes responds with content-type text/plain
+                body = JSON.parse(body);
+            } catch (e) {}
+            if (response.statusCode == 403 && quota.test(response.responseText)) {
+                throw new Error("Developer Over Qps");
+            } else {
+                callback.call(this, error, response, body);
+            }
+        });
+    } else {
+        throw new Error("Not authenticated");
+    }
+
+}
+
+module.exports = function (opts) {
+    options = opts;
+
+    if (options.token) {
+        setToken(options.token);
+    }
+
+    return {
+        isAuthenticated: isAuthenticated,
+        setToken: setToken,
+        authenticate: authenticateInplace,
+        authorizeXHR: authorizeXHR,
+        getHeaders: getHeaders,
+        getToken: getToken,
+        dropToken: dropToken,
+        getEndpoint: getEndpoint,
+        sendRequest: sendRequest
+    };
+};
