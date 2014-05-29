82504199685d7fdde0e207ef69474e77c7050221
ZB <ztenerowicz@egnyte.com>
2014-05-29 15:16:59 +0200

    cleanup

--- spec/conf/apiaccess_template.js
+++ spec/conf/apiaccess_template.js
@@ -1,4 +1,3 @@
 //Copy this file to apiaccess.js and change these mock values to real 
 egnyteDomain = "https://127.0.0.1:9999/mock/";
-//egnyteDomain = "http://10.0.0.134:8080/mock/";
 APIToken = "foo";
