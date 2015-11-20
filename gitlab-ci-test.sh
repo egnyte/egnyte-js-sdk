86339a432aed2d6e63a13f3fa7f0f8dade7060aa
ZB <ztenerowicz@egnyte.com>
2015-11-20 15:29:08 +0100

    ci setup

--- gitlab-ci-test.sh
+++ gitlab-ci-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone --depth=1 ssh://git@egnyte-git.egnyte.com:2022/integrations/pint-runner-environment.git
+cp ./pint-runner-environment/egnyte-js-sdk/access.js ./spec/conf/
 killall node || true
 npm install
 grunt test
