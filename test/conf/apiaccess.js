7008756ee65e61653f3e25843f2f900b0d093f88
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-08-25 14:58:15 +0200

    PINT-3992 forget about ignored file

--- test/conf/apiaccess.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    egnyteDomain: "https://pint.qa-egnyte.com",
-    APIToken: "5dy9eyr4paneszky2g9g2db4",
-    APIUsername: "zb",
-    OtherUsername: "dude", //other existing user (for impersonation,
-    existingFile: "/Shared/Documents/test.txt"
-};
-
